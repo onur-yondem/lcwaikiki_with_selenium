56e60e3aa919096f15221acf2770ecf407473363
Onur Can Yöndem <99740071+onur-yondem@users.noreply.github.com>
2022-06-07 02:56:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ class LCWaikiki:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
-        driver.get(self.website)
+        driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options)
+        self.driver.get(self.website)
         self.wait = WebDriverWait(self.driver, 10)
         self.actions = ActionChains(self.driver)
 
